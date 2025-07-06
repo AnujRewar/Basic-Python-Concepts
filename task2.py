baf460dfea016ba16f59fde9b53ac157815a1bcb
mr.senior <75022117+AnujRewar@users.noreply.github.com>
2025-07-06 08:59:19 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
 #greetings for user
 first_name=str(input("enter your first name"))
 last_name=str(input("enter your last name"))
-print(f"Hello, {first_name} {last_name}!. Welcome to you python program.")
+print(f"Hello, {first_name} {last_name}!. Welcome to you in python programming.")
