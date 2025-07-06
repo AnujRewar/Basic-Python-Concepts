5f3756ceded1ea95d2dc63719d20afbc56e2508e
mr.senior <75022117+AnujRewar@users.noreply.github.com>
2025-07-06 07:18:09 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#Perform Basic Mathematical Operations
+number_1=float(input("enter 1st no."))
+number_2=float(input("enter 2nd no."))
+print("addition:",number_1+number_2)
+print("subtraction:",number_1-number_2)
+print("multiply:",number_1*number_2)
+print("division:",number_1/number_2)
---
+++ task2.py
@@ -0,0 +1,4 @@
+#greetings for user
+first_name=str(input("enter your first name"))
+last_name=str(input("enter your last name"))
+print(f"Hello, {first_name} {last_name}!. Welcome to you python program.")
